47b72c7a638e417bf2ed3bdd4ff80f86f831d23f
YU-MITC <gh-cc@mlex.cc.yamaguchi-u.ac.jp>
2021-04-16 00:21:26 +0900

    Update version.php

--- version.php
+++ version.php
@@ -25,11 +25,11 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'atto_yukaltura';
-$plugin->version = 2021040800;
+$plugin->version = 2021041500;
 $plugin->release = '2.0.0';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->requires = 2015051100;
 $plugin->dependencies = array(
-    'local_yukaltura' => 2021040800,
-    'local_yumymedia' => 2021040800
+    'local_yukaltura' => 2021041500,
+    'local_yumymedia' => 2021041500
 );
