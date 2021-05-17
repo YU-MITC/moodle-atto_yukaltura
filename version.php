ac18f1f5f0a99df45bfd1ce8662bd9fee1f10eac
YU-MITC <gh-cc@mlex.cc.yamaguchi-u.ac.jp>
2021-05-18 03:47:52 +0900

    Update

--- version.php
+++ version.php
@@ -18,18 +18,18 @@
  * YU Kaltura Media for Atto, version file.
  *
  * @package   atto_yukaltura
- * @copyright (C) 2019-2020 Yamaguchi University <gh-cc@mlex.cc.yamaguchi-u.ac.jp>
+ * @copyright (C) 2019-2021 Yamaguchi University <gh-cc@mlex.cc.yamaguchi-u.ac.jp>
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'atto_yukaltura';
-$plugin->version = 2020110500;
-$plugin->release = '1.5.0';
+$plugin->version = 2021051700;
+$plugin->release = '2.0.0';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->requires = 2015051100;
 $plugin->dependencies = array(
-    'local_yukaltura' => 2020110500,
-    'local_yumymedia' => 2020110500
+    'local_yukaltura' => 2021051700,
+    'local_yumymedia' => 2021051700
 );
