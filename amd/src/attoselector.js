0d184f5b76b673a672d5411f8cf24c0a78795142
Media and Information Technology Center <31058667+YU-MITC@users.noreply.github.com>
2022-03-15 14:36:26 +0900

    Update

--- amd/src/attoselector.js
+++ amd/src/attoselector.js
@@ -16,8 +16,7 @@
 /**
  * Media Secctor script for kaltura media embed.
  *
- * @package   atto_yukaltura
- * @copyright (C) 2019-2021 Yamaguchi University (gh-cc@mlex.cc.yamaguchi-u.ac.jp)
+ * @copyright (C) 2019-2022 Yamaguchi University (gh-cc@mlex.cc.yamaguchi-u.ac.jp)
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
--- lang/en/atto_yukaltura.php
+++ lang/en/atto_yukaltura.php
@@ -18,7 +18,7 @@
  * Strings for component 'atto_yukaltura', language 'en'.
  *
  * @package   atto_yukaltura
- * @copyright (C) 2019-2021 Yamaguchi University <gh-cc@mlex.cc.yamaguchi-u.ac.jp>
+ * @copyright (C) 2019-2022 Yamaguchi University <gh-cc@mlex.cc.yamaguchi-u.ac.jp>
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
