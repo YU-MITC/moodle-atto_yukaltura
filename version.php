2c15bbc89415b4e2c7a0258137ddd50c038bbb5b
Center for Information Infrastructure <gh-cc@mlex.cc.yamaguchi-u.ac.jp>
2025-01-02 22:28:04 +0900

    Fix coding style

--- version.php
+++ version.php
@@ -32,5 +32,5 @@ $plugin->requires = 2023100900;
 $plugin->supported = [403, 405];
 $plugin->dependencies = array(
     'local_yukaltura' => 2025010200,
-    'local_yumymedia' => 2025010200
+    'local_yumymedia' => 2025010200,
 );
